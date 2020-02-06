f72b38e56617ad58fb7c07512093c9c8a4b29314
Yulong Huang <50454943+Unisonal@users.noreply.github.com>
2020-02-06 20:17:05 +0800

    changed to stacked widgets

--- yulongScratchSpace/__main__.py
+++ yulongScratchSpace/__main__.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout, QLabel, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout, QLabel, QSizePolicy, \
+    QStackedWidget
 
 application = QApplication([])
 
@@ -12,67 +13,90 @@ class Window(QWidget):
         h = self.height()
         w = self.width()
 
-        topLeft = QWidget(self)
-        topRight = QWidget(self)
-        bottomLeft = QWidget(self)
-        bottomRight = QWidget(self)
+        top_left = QWidget(self)
+        top_right = QWidget(self)
+        bottom_left = QWidget(self)
+        bottom_right = QStackedWidget(self)
 
+        #################################################
         # The top left corner
-        l = QLabel(topLeft)
+        l = QLabel(top_left)
         l.setText("Put some butterfly here")
         l.move(50, 50)
-        topLeft.resize(w * 0.25, h * 0.1)
+        top_left.resize(w * 0.25, h * 0.1)
+        #################################################
 
+        #################################################
         # The top right corner
-        l = QLabel(topRight)
+        l = QLabel(top_right)
         l.setText("Butterfly Logbook Scanner")
         l.move(w * 0.25, 50)
 
-        topRight.resize(w * 0.75, h * 0.1)
-        topRight.move(w * 0.25, 0)
+        top_right.resize(w * 0.75, h * 0.1)
+        top_right.move(w * 0.25, 0)
+        #################################################
 
+        #################################################
         # The bottom right corner
-        br_label = QLabel("Initial Page", bottomRight)
-        br_label.move(100, 100)
-        br_label.resize(500, 500)
-        bottomRight.resize(w * 0.75, h * 0.9)
-        bottomRight.move(w * 0.25, h * 0.1)
 
-        def update(message):
-            br_label.setText(message)
+        # Upload page
+        upload_page = QWidget()
+        upload_page_layout = QVBoxLayout()
 
+        QPushButton("upload",upload_page)
+
+        data_page = QWidget()
+        QPushButton("data", data_page)
+
+        tutorial_page = QWidget()
+        QPushButton("tutorial", tutorial_page)
+
+        bottom_right.addWidget(upload_page)
+        bottom_right.addWidget(data_page)
+        bottom_right.addWidget(tutorial_page)
+        bottom_right.resize(w * 0.75, h * 0.9)
+        bottom_right.move(w * 0.25, h * 0.1)
+        #################################################
+
+        #################################################
         # The bottom left corner
-        bottomLeftLayout = QVBoxLayout()
+        bottom_leftLayout = QVBoxLayout()
         buttons = [QPushButton() for _ in range(6)]
         for b in buttons:
             b.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-            bottomLeftLayout.addWidget(b)
+            bottom_leftLayout.addWidget(b)
 
+        # Upload page -- button 0
         def button0_signal():
             print("button 0 pressed")
-            update("Upload PDF page")
+            bottom_right.setCurrentIndex(0)
 
         buttons[0].setText("Upload PDF")
         buttons[0].clicked.connect(button0_signal)
 
+        # Data page -- button 1
         def button1_signal():
             print("button 1 pressed")
-            update("Access data page")
+            bottom_right.setCurrentIndex(1)
 
         buttons[1].setText("Access Data")
         buttons[1].clicked.connect(button1_signal)
 
+        # Tutorial page -- button 2
         def button2_signal():
             print("button 2 pressed")
-            update("Tutorial page")
+            bottom_right.setCurrentIndex(2)
 
         buttons[2].setText("How to Use?")
         buttons[2].clicked.connect(button2_signal)
 
-        bottomLeft.setLayout(bottomLeftLayout)
-        bottomLeft.move(0, h * 0.1)
-        bottomLeft.resize(w * 0.25, h * 0.9)
-        bottomLeft.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        bottom_left.setLayout(bottom_leftLayout)
+        bottom_left.move(0, h * 0.1)
+        bottom_left.resize(w * 0.25, h * 0.9)
+        bottom_left.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        #################################################
+
+
 
         application.setStyle('Windows')
 
