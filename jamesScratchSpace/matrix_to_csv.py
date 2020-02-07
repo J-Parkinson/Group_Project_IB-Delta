7f2249515088ec9326eb9bc5fbca329171089f03
jamesalner <jamesalner@me.com>
2020-02-07 20:39:20 +0000

    Some progress on converting to standard csv format

--- jamesScratchSpace/matrix_to_csv.py
+++ jamesScratchSpace/matrix_to_csv.py
@@ -5,7 +5,7 @@ STANDARD_HEADER = [['', '', '', 'Summary Data', '', '', '', 'Taxonomy', '', '', 
                     '', '', 'Collection', 'Label Data', 'Storage Location', '', '', '', '',
                     'Additional Notes (if additional notes become consistent, consider a new separate column'],
                    ['', '', '', 'UI Number', 'Other Number', 'Other number type', 'Type status', 'Label Family',
-                    'Label Genus', 'Label species', 'Current Family', 'Current Genus', 'Current Species', 'Subspecies',
+                    'Label Genus', 'Label species', 'Current Family', 'Current Genus', 'Current species', 'Subspecies',
                     'Common Name', '', 'Variety', 'Preservation', 'Number of specimens', 'Description', 'Sex',
                     'Stage/Phase', '', 'Condition Rating (Good, Fair, Poor, Unacceptable)',
                     'Condition details (eg wing fallen off)', 'Level 1 eg.Country', 'Level 2 - eg.County',
@@ -29,19 +29,45 @@ STANDARD_HEADER = [['', '', '', 'Summary Data', '', '', '', 'Taxonomy', '', '', 
                     'LocCurrentLocationRef.LocLevel4', 'LocMovementNotes', 'NotNotes']]
 
 
+def split_words(table, field_name, new_cols):
+    field_index = table[0].index(field_name)
+    for row_index, row in enumerate(table):
+        if row_index != 0:
+            words = row[field_index].split()
+            row += words
+        else:
+            row += new_cols
+
+
 def matrix_to_csv(table, path):
     with open(path, mode='w') as outfile:
         out = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        for row in table:
-            out.writerow(row)
+        out.writerows(table)
 
 
-def matrix_to_standard_csv(table, path):
+def matrix_to_standard(table, field_map):
+    # the field map will map the standard field headers to table's field headers
+    result = [["" for _ in range(len(STANDARD_HEADER[0]))] for _ in range(len(table) - 1)]
+    for field in STANDARD_HEADER[1]:
+        if field in field_map:
+            std_field_index = STANDARD_HEADER[1].index(field)
+            table_field_index = table[0].index(field_map[field])
+            for row_num, row in enumerate(result):
+                row[std_field_index] = table[row_num + 1][table_field_index]
+    for field in STANDARD_HEADER[2]:
+        if field in field_map:
+            std_field_index = STANDARD_HEADER[2].index(field)
+            table_field_index = table[0].index(field_map[field])
+            for row_num, row in enumerate(result):
+                row[std_field_index] = table[row_num + 1][table_field_index]
+    return result
+
+
+def matrix_to_standard_csv(table, path, field_map):
     with open(path, mode='w') as outfile:
         out = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         out.writerows(STANDARD_HEADER)
-        for row in table:
-            out.writerow(row)
+        out.writerows(matrix_to_standard(table, field_map))
 
 
 test = [[f'({x} {y})' for x in range(10)] for y in range(10)]
@@ -59,4 +85,8 @@ std_test = [['Invertebrates; Insects', 'Object', 'Present', 'I.2019.2147', '', '
 
 matrix_to_csv(test, './jamesScratchSpace/test.csv')
 matrix_to_csv(test2, './jamesScratchSpace/test2.csv')
-matrix_to_standard_csv(std_test, './jamesScratchSpace/std_test.csv')
+
+
+split_words(test2, 'Present Determination', ['Genus', 'Species'])
+matrix_to_standard_csv(test2, './jamesScratchSpace/std_test.csv', {'Current Genus': 'Genus',
+                                                                   'Current species': 'Species'})
