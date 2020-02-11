066da20d1805b2591c3e8e2b8be483fde520742d
abirw <abicarver13@gmail.com>
2020-02-11 23:07:40 +0000

    Unfinished word recognition but getting there

--- scratchSpaces/abiScratchSpace/CellToWords.py
+++ scratchSpaces/abiScratchSpace/CellToWords.py
@@ -13,14 +13,20 @@ arr.shape = (height, width, channels)
 arr = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
 
 def cellToWords(cell): # takes one numpy array
-    h,w = cell.shape
-    numBlackPix = np.sum(cell, axis=0)
-    maxVal = np.amax(numBlackPix)
-    difference = np.subtract(np.full(numBlackPix.shape, maxVal), numBlackPix)
-    x =np.gradient(numBlackPix)
-    x[x > 4] = 10
-    y = ndimage.convolve1d(numBlackPix, np.array(1,1,1,1), mode="nearest")
-    plt.plot(y)
+    newImages = []
+    columnVals = np.sum(cell, axis=0)
+    maxValCol = np.amax(columnVals)
+    x = ndimage.convolve1d(columnVals, np.array([1,1,1,1]), mode="nearest")
+    print(x.shape)
+    indicesToSplit = np.array(np.where(x >0.95*maxValCol))
+    indicesToSplit = np.ndarray.flatten(indicesToSplit)
+    #indicesToSplit.astype(int)
+    words = np.split(x, indicesToSplit)
+
+    rowVals = np.sum(cell, axis=1)
+    maxValRow = np.amax(rowVals)
+    y = ndimage.convolve1d(rowVals, np.array([1, 1, 1, 1]), mode="nearest")
+    #plt.plot(x)
     plt.show()
 
 
