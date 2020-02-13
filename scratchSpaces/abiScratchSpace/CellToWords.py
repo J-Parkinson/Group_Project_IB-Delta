427098509d9c5871eec0b449f1518beaa193eac2
abirw <abicarver13@gmail.com>
2020-02-13 20:44:23 +0000

    Word recognition **MISSING TEMP FILES**

--- scratchSpaces/abiScratchSpace/CellToWords.py
+++ scratchSpaces/abiScratchSpace/CellToWords.py
@@ -13,21 +13,53 @@ arr.shape = (height, width, channels)
 arr = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
 
 def cellToWords(cell): # takes one numpy array
-    newImages = []
-    columnVals = np.sum(cell, axis=0)
-    maxValCol = np.amax(columnVals)
-    x = ndimage.convolve1d(columnVals, np.array([1,1,1,1]), mode="nearest")
-    print(x.shape)
-    indicesToSplit = np.array(np.where(x >0.95*maxValCol))
-    indicesToSplit = np.ndarray.flatten(indicesToSplit)
-    #indicesToSplit.astype(int)
-    words = np.split(x, indicesToSplit)
+    newWords = []
+    rows = cellToRows(cell)
+    for row in rows:
+        words = rowToWords(row)
+        for word in words:
+            newWords.append(removeWhiteSpaceFromWord(word))
 
+    # **do the stuff with the temp directory here**
+
+
+def rowToWords(row):
+    columnVals = np.sum(row, axis=0)
+    maxValCol = np.amax(columnVals)
+    x = ndimage.convolve1d(columnVals, np.array([1, 1, 1, 1]), mode="nearest")
+
+    indicesToSplit = np.array(np.where(x > 0.95 * maxValCol))
+    indicesToSplit = np.ndarray.flatten(indicesToSplit)
+
+    words = np.split(x, indicesToSplit)
+    words = [word for word in words if word.size > 0]
+    return words
+
+
+def cellToRows(cell):
     rowVals = np.sum(cell, axis=1)
     maxValRow = np.amax(rowVals)
-    y = ndimage.convolve1d(rowVals, np.array([1, 1, 1, 1]), mode="nearest")
-    #plt.plot(x)
-    plt.show()
+    x = ndimage.convolve1d(rowVals, np.array([1, 1, 1, 1]), mode="nearest")
+
+    indicesToSplit = np.array(np.where(x > 0.95 * maxValRow))
+    indicesToSplit = np.ndarray.flatten(indicesToSplit)
+
+    rows = np.split(x, indicesToSplit)
+    rows = [row for row in rows if row.size > 0]
+    return rows
+
+
+def removeWhiteSpaceFromWord(word):
+    horizontalVals = np.sum(word, axis=1)
+    maxValHorizontal = np.amax(horizontalVals)
+    x = ndimage.convolve1d(horizontalVals, np.array([1, 1, 1, 1]), mode="nearest")
+
+    indicesToSplit = np.array(np.where(x > 0.95 * maxValHorizontal))
+    indicesToSplit = np.ndarray.flatten(indicesToSplit)
+
+    word = np.split(x, indicesToSplit)
+    word = [row for row in rows if row.size > 0]
+    return word
 
 
 cellToWords(arr)
